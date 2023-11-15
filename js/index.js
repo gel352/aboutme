9e1f21c11bf0894b6acb0e42cbb777ebaaedaef9
waeds <120262123+gel352@users.noreply.github.com>
2023-11-15 21:57:33 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,19 +1,25 @@
 var zfb = document.getElementById('zfb');
 var wx = document.getElementById('wx');
+var qq = document.getElementById('qq');
 var shows = document.querySelector('.shws');
 var showd = document.querySelector('.shwd');
+var showa = document.querySelector('.shwa');
 $(wx).click(function(){
   
   $(shows).fadeIn(260);
   $(showd).fadeOut(260);
+  $(showa).fadeOut(260);
 });
 $(zfb).click(function(){
-
+  $(showa).fadeOut(260);
   $(shows).fadeOut(260);
   $(showd).fadeIn(260);
-
 });
-
+$(qq).click(function(){
+  $(showd).fadeOut(260);
+  $(shows).fadeOut(260);
+  $(showa).fadeIn(260);
+});
 var out = document.getElementById('ou');
 $(out).click(function(){
   $('.toolk').fadeOut(160);
