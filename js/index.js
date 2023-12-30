6e057d4e48fa5726d4e6543d7db1c6a9b2ae0702
waeds <120262123+gel352@users.noreply.github.com>
2023-12-31 07:08:46 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,32 +1,70 @@
-var zfb = document.getElementById('zfb');
-var wx = document.getElementById('wx');
-var qq = document.getElementById('qq');
-var shows = document.querySelector('.shws');
-var showd = document.querySelector('.shwd');
-var showa = document.querySelector('.shwa');
-$(wx).click(function(){
+$(function() {
+  //微信支付
+  $('#wx').click(function() {
+    var wm = new Audio('mp3/Wm.mp3');
+    wm.play();
+    
+    setTimeout(function () {
+      var sk = new Audio('mp3/shake.mp3');
+    sk.play();
+    },1000);
+    
+    $('.wxzf').animate({
+      top: 50,
+      opacity: 1,
+    }, 1600);
+    $('.zfbzf,.qqzf').animate({
+      top: -420,
+      opacity: 0,
+    }, 1500);
+  });
+  //支付宝支付
+  $('#zfb').click(function() {
+    var wm = new Audio('mp3/Wm.mp3');
+    wm.play();
+    
+    setTimeout(function () {
+      var sk = new Audio('mp3/shake.mp3');
+    sk.play();
+    },1000);
+    
+    $('.zfbzf').animate({
+      top: 50,
+      opacity: 1,
+    }, 1600);
+    $('.wxzf,.qqzf').animate({
+      top: -420,
+      opacity: 0,
+    }, 1500);
+  });
+  //QQ支付
+  $('#qq').click(function() {
+    var wm = new Audio('mp3/Wm.mp3');
+    wm.play();
+    setTimeout(function () {
+      var sk = new Audio('mp3/shake.mp3');
+    sk.play();
+    },1000);
+    $('.qqzf').animate({
+      top: 50,
+      opacity: 1,
+    }, 1600);
+    $('.wxzf, .zfbzf').animate({
+      top: -420,
+      opacity: 0,
+    }, 1500);
+  });
+  //收款码弹窗
+  $('#souk').click(function(){
+    $('#toolk').fadeIn(400);
+    var ck = new Audio('mp3/ck.ogg');
+    ck.play();
+  });
+  //收款码关闭
+  $('#ou').click(function(){
+    $('#toolk').fadeOut(400);
+    var ck = new Audio('mp3/playend.mp3');
+    ck.play();
+  });
   
-  $(shows).fadeIn(260);
-  $(showd).fadeOut(260);
-  $(showa).fadeOut(260);
-});
-$(zfb).click(function(){
-  $(showa).fadeOut(260);
-  $(shows).fadeOut(260);
-  $(showd).fadeIn(260);
-});
-$(qq).click(function(){
-  $(showd).fadeOut(260);
-  $(shows).fadeOut(260);
-  $(showa).fadeIn(260);
-});
-var out = document.getElementById('ou');
-$(out).click(function(){
-  $('.toolk').fadeOut(160);
-})
-var sou = document.getElementById('souk');
-$(sou).click(function(){
-  setTimeout(function(){
-  $('.toolk').fadeIn(160);
-  },200);
 });
